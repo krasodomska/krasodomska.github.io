849c35ef9a0ec97441029fc53b9b1f3d92493a9d
krasodomska <39217554+krasodomska@users.noreply.github.com>
2018-05-23 22:50:35 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include "templates/header.php"; ?>
+<?php include "templates/element1.php"; ?>
+<?php include "templates/element2.php"; ?>
+<?php include "templates/element3.php"; ?>
+<?php include "templates/footer.php"; ?>
