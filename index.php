c89bd5e3d975cafbf3cef13c5c229b24e8e783f6
krasodomska <39217554+krasodomska@users.noreply.github.com>
2018-06-07 14:00:30 +0200

    Delete index.php

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php include "templates/header.php"; ?>
-<?php include "templates/element1.php"; ?>
-<?php include "templates/element2.php"; ?>
-<?php include "templates/element3.php"; ?>
-<?php include "templates/footer.php"; ?>
